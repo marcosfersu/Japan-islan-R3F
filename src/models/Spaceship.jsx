65a6d8c3bd671c3696764693a8a68ea566523348
marcos.fersu <marcos.fersu@gmail.com>
2023-12-01 15:25:18 -0300

    add animation ship

--- src/models/Spaceship.jsx
+++ src/models/Spaceship.jsx
@@ -1,67 +1,97 @@
 import { useRef, useState, useEffect } from "react";
 
-import spaceship from "../assets/spaceship2.glb";
+import spaceship from "../assets/spaceship.glb";
 import { useAnimations, useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 
 const Spaceship = ({ isRotating, setIsRotating, limitRotation, ...props }) => {
-  const spaceshipRef = useRef();
-  const { scene, animations } = useGLTF(spaceship);
-  const { actions } = useAnimations(animations, spaceshipRef);
+	const spaceshipRef = useRef();
+	const { scene, animations } = useGLTF(spaceship);
+	const { actions } = useAnimations(animations, spaceshipRef);
 
-  const { gl, viewport } = useThree();
+	const { gl, viewport } = useThree();
 
-  const lastY = useRef(0);
+	const lastY = useRef(0);
 
-  const handlePointerDown = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
+	const handlePointerDown = e => {
+		e.stopPropagation();
+		e.preventDefault();
 
-    const clientY = event.touches ? event.touches[0].clientY : event.clientY;
+		const clientY = event.touches ? event.touches[0].clientY : event.clientY;
 
-    lastY.current = clientY;
-  };
+		lastY.current = clientY;
+	};
 
-  const handlePointerMove = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    if (isRotating) {
-      const clientY = event.touches ? event.touches[0].clientY : event.clientY;
+	const handlePointerMove = e => {
+		e.stopPropagation();
+		e.preventDefault();
+		if (isRotating) {
+			const clientY = event.touches ? event.touches[0].clientY : event.clientY;
 
-      const deltaY = (clientY - lastY.current) / viewport.height;
+			const deltaY = (clientY - lastY.current) / viewport.height;
 
-      if (limitRotation <= 0.4 && limitRotation >= 0) {
-        spaceshipRef.current.position.y -= deltaY * 0.035 * Math.PI;
-        spaceshipRef.current.rotation.x += deltaY * 0.01 * Math.PI;
-      }
+			if (limitRotation <= 0.4 && limitRotation >= 0) {
+				spaceshipRef.current.position.y -= deltaY * 0.035 * Math.PI;
+				spaceshipRef.current.rotation.x += deltaY * 0.01 * Math.PI;
+			}
 
-      lastY.current = clientY;
-    }
-  };
+			lastY.current = clientY;
+		}
+	};
 
-  useEffect(() => {
-    const canvas = gl.domElement;
-    canvas.addEventListener("pointerdown", handlePointerDown);
-    canvas.addEventListener("pointermove", handlePointerMove);
+	useFrame(() => {
+		isRotating
+			? scene.rotation.x > Math.PI * -0.5
+				? (scene.rotation.x -= 0.2)
+				: ""
+			: scene.rotation.x <= -0.2
+			? (scene.rotation.x += 0.2)
+			: "";
+	});
 
-    console.log(actions);
-    if (isRotating) {
-      actions["rockedAction.001"].play();
-      actions["partcles"].play();
-      actions["partcles.001"].play();
-    }
+	useEffect(() => {
+		const canvas = gl.domElement;
+		canvas.addEventListener("pointerdown", handlePointerDown);
+		canvas.addEventListener("pointermove", handlePointerMove);
 
-    return () => {
-      canvas.removeEventListener("pointerdown", handlePointerDown);
-      canvas.removeEventListener("pointermove", handlePointerMove);
-    };
-  }, [gl, handlePointerDown, handlePointerMove]);
+		if (isRotating) {
+			actions["rockedAction.001"].play();
+			actions["partcles.006"].play();
+			actions["partcles.007"].play();
+			actions["partcles.008"].play();
+			actions["partcles.010"].play();
+			actions["partcles.1"].play();
+			actions["stop"].stop();
+			actions["stop-fire"].stop();
+			// scene.rotation.set(Math.PI * 0.4, Math.PI, Math.PI * 0.5);
+		} else {
+			actions["rockedAction.001"].stop();
+			actions["partcles.006"].stop();
+			actions["partcles.007"].stop();
+			actions["partcles.008"].stop();
+			actions["partcles.010"].stop();
+			actions["partcles.1"].stop();
+			actions["stop"].play();
+			actions["stop-fire"].play();
+			scene.children[0].scale.set(0);
+			scene.children[1].scale.set(0);
+			scene.children[2].scale.set(0);
+			scene.children[3].scale.set(0);
+			scene.children[4].scale.set(0);
+			// scene.rotation.set(Math.PI * 0.4, Math.PI * -0.5, Math.PI * 0.5);
+		}
 
-  return (
-    <mesh ref={spaceshipRef} {...props}>
-      <primitive object={scene} />
-    </mesh>
-  );
+		return () => {
+			canvas.removeEventListener("pointerdown", handlePointerDown);
+			canvas.removeEventListener("pointermove", handlePointerMove);
+		};
+	}, [gl, handlePointerDown, handlePointerMove]);
+
+	return (
+		<mesh ref={spaceshipRef} {...props}>
+			<primitive object={scene} />
+		</mesh>
+	);
 };
 
 export default Spaceship;
